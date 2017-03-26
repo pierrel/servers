54558ee15c2e5f75068c20ad4534609c32515dac
Pierre Larochelle <pierrebombay@gmail.com>
2017-03-26 23:14:03 +0000

    Adding dynamo support

--- handler.js
+++ handler.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -10,6 +15,46 @@ module.exports.hello = (event, context, callback) => {
 
   callback(null, response);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports.create_post = (event, context, callback) => {
+  const data = JSON.parse(event.body);
+  const requirements = ['content', 'type', 'author'];
+
+  for (var i = 0; i < requirements.length; i++) {
+    const requirement = requirements[i];
+    if (typeof data[requirement] !== 'string') {
+      console.error('Validation failed!');
+      callback(new Error("Couldn't create post"));
+      return;
+    }
+  }
+  const timestamp = data.timestamp || new Date().getTime();
+
+  const params = {
+    TableName: process.env.POSTS_TABLE,
+    Item: {
+      id: uuid.v1(),
+      content: data.content,
+      author: data.author,
+      type: data.type,
+      createAt: timestamp,
+      updatedAt: timestamp
+    }
+  };
+
+  dynamoDb.put(params, (error, result) => {
+    if (error) {
+      console.error("Adding post: ", error);
+      callback(new Error("Couldn't create the post"));
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item)
+    };
+
+    callback(null, response);
+  });
 };
