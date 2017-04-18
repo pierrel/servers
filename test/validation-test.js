7e81bdf4f4b4e2f487c04608d6fb4f0e114d726b
Pierre Larochelle <pierrebombay@gmail.com>
2017-04-18 04:30:25 +0000

    correct validation

--- test/validation-test.js
+++ test/validation-test.js
@@ -1,9 +1,11 @@
 const validate = require('../lib/validation.js');
 
-const qunit = Qunit; // eslint-disable-line no-undef
+const qunit = QUnit; // eslint-disable-line no-undef
 
 qunit.test('validation correctly validates a post', (assert) => {
-  assert.equal(validate.post_create({
-    something: 'else',
-  }), undefined);
+  assert.equal(validate.postCreate({
+    author: 'New Amaral',
+    content: 'https://github.com/serverless/examples/tree/master/aws-node-rest-api-with-dynamodb',
+    type: 'link',
+  }, undefined));
 });
